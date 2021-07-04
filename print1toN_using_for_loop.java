c9cc8dc978138ac5713ef8b2e3561cf870b0a0b1
shalovictor <86366745+shalovictor@users.noreply.github.com>
2021-07-05 00:22:00 +0530

    Create print1toN_using_for_loop.java

---
+++ print1toN_using_for_loop.java
@@ -0,0 +1,11 @@
+public class print1toN_using_for_loop
+{
+  public static void main(String args[])
+  {
+    int n=100;
+    for (int i=1;i<=n;i++)
+    {
+      System.out.println(i);
+    }
+  }
+}
