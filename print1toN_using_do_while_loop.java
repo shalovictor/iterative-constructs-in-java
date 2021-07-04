f1a30b21aba4af0bfd09c1281b7afdd101574d70
shalovictor <86366745+shalovictor@users.noreply.github.com>
2021-07-05 00:27:16 +0530

    Create print1toN_using_do_while_loop.java

---
+++ print1toN_using_do_while_loop.java
@@ -0,0 +1,13 @@
+public class print1toN_using_do_while_loop
+{
+  public static void main(String args[])
+  {
+    int n=100;
+    int i=1;
+    do
+    {
+      System.out.println(i);
+      i++;
+    } while ( i<=n);
+  }
+}
